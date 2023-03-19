32211ed8202835efc0acf52fff600d3dfd757c9a
Robert Tan <sanityrisen@hotmail.com>
2023-03-20 05:47:23 +1000

    updates

---
+++ MyResearch.js
@@ -0,0 +1,14 @@
+
+
+
+
+  function scrollToSection2() {
+    const section2 = document.querySelector('#section2');
+    section2.scrollIntoView({ behavior: 'smooth', duration: 2500 });
+  }
+  
+
+
+
+
+
